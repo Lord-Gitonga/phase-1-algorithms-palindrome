07c3fe588b2d23d958218cdacd6a49a5adfa00c0
lordish <gitonga403@gmail.com>
2023-03-30 06:21:27 +0300

    Palindrome

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLowerCase ();
+  for(let i=0, j=word.length-1; i<j; i++, j--){
+    if(word[i] != word[j]) {
+      return false;
+    } else { return true; }
+  } 
 }
-
+console.log(isPalindrome("racecar"))
+console.log(isPalindrome("horse"))
 /* 
   Add your pseudocode here
+  first put all words to lowercase
+  add a for loop to my string to see if it matches my palindome expectations.
+  console log it to see my solution.
 */
 
 /*
   Add written explanation of your solution here
+  I pass a for loop to loop my string from both ends to see if they match at any way.
 */
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
